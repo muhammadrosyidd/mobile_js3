7b56349da1eac5b590ca5327d046ef0bba0ff997
muhammadrosyidd <muhammadrosyidd>
2025-09-24 18:51:00 +0700

    first commit

---
+++ bin/faktorial.dart
@@ -0,0 +1,5 @@
+import 'package:faktorial/faktorial.dart' as faktorial;
+
+void main(List<String> arguments) {
+  print('Hello world: ${faktorial.calculate()}!');
+}
---
+++ test/faktorial_test.dart
@@ -0,0 +1,8 @@
+import 'package:faktorial/faktorial.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
